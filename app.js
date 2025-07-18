518d0b641fcef74f1b93da491ae8be023b02201b
manuelduran <mata.duran@gmail.com>
2025-07-18 23:16:00 +0000

    add ymal3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = process.env.PORT || 8080; // Use App Engine's provided PORT
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  const response = { message: 'Hello, World! 2' };
+  const response = { message: 'Hello, World!' };
   res.end(JSON.stringify(response));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
