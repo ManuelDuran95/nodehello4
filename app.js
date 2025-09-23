bce5138149439919d6ec538c68df46cb9c20b5e4
Manuel Durán <mata.duran@gmail.com>
2025-09-22 18:27:11 -0600

    Log Node.js version on server request
    Log the current Node.js version when the server receives a request.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 8080; // Use App Engine's provided PORT
 
 const server = http.createServer((req, res) => {
+  
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
+  console.log("verions of nodejs", process.version);
   const response = { message: 'Hello, World!' };
   res.end(JSON.stringify(response));
 });
