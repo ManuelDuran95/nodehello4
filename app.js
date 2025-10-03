5fab53c4e92a76aa0057058c942e69383db6273b
Manuel Durán <mata.duran@gmail.com>
2025-10-03 13:21:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 8080; // Use App Engine's provided PORT
 
 const server = http.createServer((req, res) => {
+  console.log("quizarIRU", "https://quizapp-frontend-974768286444.us-central1.run.app/home");
   
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
