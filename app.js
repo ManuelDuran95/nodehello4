90e523c13545cc8bd22d366afd6614349a69f342
Manuel Durán <mata.duran@gmail.com>
2025-12-24 15:37:15 -0600

    Update response message to 'Hello, World! 2'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   console.log("verions of nodejs s", process.version);
-  const response = { message: 'Hello, World!' };
+  const response = { message: 'Hello, World! 2' };
   res.end(JSON.stringify(response));
 });
 
